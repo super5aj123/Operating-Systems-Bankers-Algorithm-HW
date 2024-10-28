065012a550c05faa0972e5034bef49b8cfbf51bd
Anthony Rowan <super5aj12345@outlook.com>
2024-10-28 16:06:35 -0400

    Added code to get the number of currently allocated resources to each process.

--- Program.cs
+++ Program.cs
@@ -6,18 +6,28 @@ using System.Numerics;
 
 int numProcesses; //Variable to hold the total number of processes
 int numResources; //Variable to hold the total number of different resources
-StreamReader read = new StreamReader("data.txt");
-string temp;
+StreamReader read = new StreamReader("data.txt"); //StreamReader object used to read the file
+string temp; //String to temporarially hold various values.
 
-numProcesses = int.Parse(read.ReadLine());
-numResources = int.Parse(read.ReadLine());
+numProcesses = int.Parse(read.ReadLine()); //Read the first line to get the number of processes
+numResources = int.Parse(read.ReadLine()); //Read the second line to get the number of resources
 
 temp = read.ReadLine();
 string[] numHolder = temp.Split(' ');
-int[] resourceTracker = new int[numResources]; //Vector to hold the current count of avaliable instances of the different resources
+int[] resourceTracker = new int[numResources]; //Array to hold the current count of avaliable instances of the different resources
 for (int i = 0; i < numResources; i++)
 {
     resourceTracker[i] = int.Parse(numHolder[i]);
 }
 
-int[,] processTracker;
+int[,] processTracker = new int [numProcesses,numResources];
+
+for(int i = 0;i < numProcesses;i++)
+{
+    temp = read.ReadLine();
+    numHolder = temp.Split(' ');
+    for(int j = 0; j<numResources; j++)
+    {
+        processTracker[i,j] = int.Parse(numHolder[j]);
+    }
+}
